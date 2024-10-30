f890436cc3d140e4100bddb7c57913dfdc75e56f
pmalimba <pmalimba@yahoo.com>
2024-10-30 15:46:06 +0100

    added basic form handling and javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateVerse(event) {
+  event.preventDefault();
 
+  new Typewriter("#verse", {
+    strings: "Be still, and know that I'm God",
+    autoStart: true,
+    Cursor: "",
+    delay: 1,
+  });
+}
+
+let verseFormElement = document.querySelector("#verse-generator-form");
+verseFormElement.addEventListener("submit", generateVerse);
